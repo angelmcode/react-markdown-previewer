540ab89e4e0ad1fddf06719cd4f848f5ba9cd9b9
angelmcode <135392400+AngelMCode@users.noreply.github.com>
2023-07-10 21:30:56 -0600

    react markdown previewer update 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -48,6 +48,9 @@ function App() {
   
   ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
   ![React Logo](https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png)
+  ![html Logo](https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png)
+  ![css Logo](https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png)
+  ![javascript Logo](https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png)
   `
 
   const initialInput = a;
